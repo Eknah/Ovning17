8a352ec14bd41fee468760c0fd45b4d693db5268
Eknah <eknah@protonmail.com>
2022-10-04 16:17:55 +0200

    Add project files.

---
+++ Ovnign17.Shared/Device.cs
@@ -0,0 +1,11 @@
+﻿namespace Ovnign17.Shared
+{
+    public class Device
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString("n");
+        public Location? Location { get; set; } = default;
+        public DateOnly LastUpdated { get; set; } = default;
+        public DeviceType? Type { get; set; } = default;
+        public bool IsOnline { get; set; } = false;
+    }
+}
---
+++ Ovnign17.Shared/Location.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovnign17.Shared
+{
+    public class Location
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+    }
+}
