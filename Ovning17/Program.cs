7d7c7a0a934ed0d744b83d388767f2e6c4c82557
Eknah <eknah@protonmail.com>
2022-10-04 16:56:44 +0200

    Register httpclient service

--- Ovning17/Program.cs
+++ Ovning17/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Ovning17;
+using Ovning17.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddHttpClient<IDevicesClient, DevicesClient>(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
 await builder.Build().RunAsync();
